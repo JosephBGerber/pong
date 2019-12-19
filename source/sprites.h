a3b54d5c4f1a2a7251e41fc85d37e427fd301fcf
joseph <josephbgerber@gmail.com>
2019-12-19 00:38:10 -0500

    Initial commit

---
+++ source/sprites.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(sprites)
+
+//======================================================================
+//
+//	sprites, 16x16@4, 
+//	+ palette 256 entries, not compressed
+//	+ 4 tiles Metatiled by 2x2 not compressed
+//	Total size: 512 + 128 = 640
+//
+//	Time-stamp: 2019-12-19, 00:34:14
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.15
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_SPRITES_H
+#define GRIT_SPRITES_H
+
+#define spritesTilesLen 128
+extern const unsigned int spritesTiles[32];
+
+#define spritesPalLen 512
+extern const unsigned short spritesPal[256];
+
+#endif // GRIT_SPRITES_H
+
+//}}BLOCK(sprites)
